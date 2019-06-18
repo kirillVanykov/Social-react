5612b5148a2474a4ca76ac8014906b6f813c67a7
Kirill Vanykov <kiryshavanykov@gmail.com>
2019-06-19 00:00:04 +0300

    Messages icons

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,10 +3,10 @@ import s from './../Dialogs.module.css';
 
 const Message = (props) => {
   const whoWriting1 = props.whoWriting;
-  
+
 
     return (
-      <div className={`${s.whoWriting1}`}>
+      <div className={whoWriting1}>
         <div className={s.iconMessage}></div>
         <div className={s.message}>{props.message}</div>
       </div>
